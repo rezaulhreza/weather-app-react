8a4983e33743c010cb4534ff4c4db1192ccadb1a
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2020-08-07 00:05:25 +0100

    create datebuilder

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React from "react";
 const api = {
   key: "834 adefb124939779388d6a4b07b0e08",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-  return ( <
-    div className = "app warm" >
-    <
-    main >
-    <
-    div className = "search-box" >
-    <
-    input type = "text"
-    className = "search-bar"
-    placeholder = "Search.." / >
-    <
-    /div> < /
-    main > <
-    /div>
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  };
+
+  return (
+    <div className="app warm">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search.." />
+        </div>
+        <div className="location-box">
+          <div className="location">New York,USA</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+      </main>
+    </div>
   );
 }
 
