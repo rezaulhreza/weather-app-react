674df2ae3362c453da53a4b0e7eb53c6498a06eb
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2020-08-06 23:01:58 +0100

    started developing

---
+++ weather-react/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+const api = {
+  key: "834 adefb124939779388d6a4b07b0e08",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+  return ( <
+    div className = "app warm" >
+    <
+    main >
+    <
+    div className = "search-box" >
+    <
+    input type = "text"
+    className = "search-bar"
+    placeholder = "Search.." / >
+    <
+    /div> < /
+    main > <
+    /div>
+  );
+}
+
+export default App;
